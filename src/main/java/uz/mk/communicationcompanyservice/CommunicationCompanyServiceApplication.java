5e7dff8378baa492ef07844c36056e796f2d52df
mirzokhidkhasanov <mirzohidkhasanov12@gmail.com>
2021-07-05 22:26:06 +0500

    started to build project

---
+++ src/main/java/uz/mk/communicationcompanyservice/CommunicationCompanyServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.mk.communicationcompanyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationCompanyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunicationCompanyServiceApplication.class, args);
+    }
+
+}
